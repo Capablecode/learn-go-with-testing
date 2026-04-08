e6a9a9301c3549eaf53e84069df61ffa6b4c1725
salabi <alabisundayayofe@gmail.com>
2026-04-08 16:36:53 +0100

    Day 1, learning go with testing

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"learning/lesson"
+)
+
+func main() {
+	fmt.Println(lesson.Factorial_Recursion(5))
+	fmt.Println(lesson.ReverseAString("hello"))
+	fmt.Println(lesson.Hello("Eldee","Spanish"))
+
+}

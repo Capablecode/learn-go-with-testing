b80e7354f829482f150f4f677175d5b525342836
salabi <alabisundayayofe@gmail.com>
2026-04-14 16:56:44 +0100

    method, struct

---
+++ shape/shapes.go
@@ -0,0 +1,28 @@
+package shape
+
+import "math"
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+// rectangle method
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+// circle method
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+
+
+// func Perimeter(r Rectangle) float64 {
+// 	return 2 * (r.Width + r.Height)
+// }
---
+++ shape/shapes_test.go
@@ -0,0 +1,41 @@
+package shape
+
+import (
+	"testing"
+)
+
+// func TestPerimeter(t *testing.T) {
+// 	t.Run("Perimeter of a rectangle", func(t *testing.T) {
+
+// 		rectangle := Rectangle{10.0, 10.0}
+// 		got := Perimeter(rectangle)
+// 		expected := 40.0
+
+// 		if got != expected {
+// 			t.Errorf("got %.2f but expected %.2f", got, expected)
+// 		}
+// 	})
+// }
+
+func TestArea(t *testing.T) {
+	t.Run("Area of Rectangle", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		got := rectangle.Area()
+		expected := 72.0
+
+		if got != expected {
+			t.Errorf("got %.2f but expected %.2f", got, expected)
+		}
+	})
+
+	t.Run("Area of a Circle", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		expected := 314.1592653589793
+
+		if got != expected {
+			t.Errorf("got %g but expected %g", got, expected)
+		}
+	})
+
+}

eccce36c4ea58391156a008c4877ceca4ca8b78c
salabi <alabisundayayofe@gmail.com>
2026-04-15 16:29:54 +0100

    interface

---
+++ wallet/wallet.go
@@ -0,0 +1,14 @@
+package wallet
+
+// type Wallet struct {
+// 	// Deposit float64
+// 	// Balance float64
+// }
+
+func (w Wallet) Deposit(amount int) {
+
+}
+
+func (w Wallet) Balance() int {
+	return 0
+}

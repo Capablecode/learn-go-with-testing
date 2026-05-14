dd86a3e5633c7e3f058c383b6c92f24e07dcf43f
salabi <alabisundayayofe@gmail.com>
2026-05-14 16:33:30 +0100

    ascii web work added

--- ascii-art-web/printasciiart.go
+++ ascii-art-web/printasciiart.go
@@ -3,17 +3,19 @@ package asciiartweb
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 	"os"
 	"strings"
 )
 
 func PrintAscii(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost{
-		http.Error(w, "Bad Request", http.StatusBadRequest)
-	}
+	// if r.Method != http.MethodPost {
+	// 	http.Error(w, "Bad Request", http.StatusBadRequest)
+	// }
 	text := r.FormValue("text")
-	banner := r.FormValue("banner")
+	banner := r.FormValue("banners")
+	log.Println(text, banner)
 	// text := "hello"
 	// banner := "standard.txt"
 
@@ -24,10 +26,9 @@ func PrintAscii(w http.ResponseWriter, r *http.Request) {
 
 	newText := strings.Split(text, "\n")
 	banners, err := ReadFile(banner)
-	if err != nil{
+	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-	} 
-
+	}
 
 	result, err := GenerateAsciiArt(newText, banners)
 	if err != nil {
@@ -36,15 +37,15 @@ func PrintAscii(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(result))
 }
 
-func ServeHome(w http.ResponseWriter, r *http.Request){
+func ServeHome(w http.ResponseWriter, r *http.Request) {
 	templ, err := template.ParseFiles("ascii-art-web/template/index.html")
-	if err != nil{
+	if err != nil {
 		http.Error(w, "Template not found", http.StatusInternalServerError)
 		return
 	}
 
 	err = templ.Execute(w, nil)
-	if err != nil{
+	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -59,22 +60,22 @@ func GenerateAsciiArt(text, banner []string) (string, error) {
 			continue
 		}
 
-	//the i handles the vertical rows of the font
-	for i := 1; i <= 8; i++ {
-		//the j handles the horizontal movement of characters in the word
-		for j := 0; j < len(text[k]); j++ {
-			if text[k][j] < 32 || text[k][j] > 126 {
-				return "", fmt.Errorf("found unprintable character")
+		//the i handles the vertical rows of the font
+		for i := 1; i <= 8; i++ {
+			//the j handles the horizontal movement of characters in the word
+			for j := 0; j < len(text[k]); j++ {
+				if text[k][j] < 32 || text[k][j] > 126 {
+					return "", fmt.Errorf("found unprintable character")
+				}
+				character_index := int(text[k][j] - 32)
+				start := (character_index * 9) + i
+				result.WriteString((banner[start]))
 			}
-			character_index := int(text[k][j] - 32)
-			start := (character_index * 9) + i
-			result.WriteString((banner[start]))
-		} 
-		//Move to a new line to continue printing the block of character line by line
-		// fmt.Println()
-		result.WriteString("\n")
+			//Move to a new line to continue printing the block of character line by line
+			// fmt.Println()
+			result.WriteString("\n")
+		}
 	}
-}
 	return result.String(), nil
 }
 
@@ -87,4 +88,3 @@ func ReadFile(path string) ([]string, error) {
 	result := strings.Split(files, "\n")
 	return result, nil
 }
-
